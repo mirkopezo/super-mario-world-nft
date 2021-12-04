ad231148645db06dda413f0c4468575536959bf6
mirkopezo <accfireover@tutanota.com>
2021-12-05 00:43:48 +0100

    Create setApprovalForAll and isApprovedForAll functions

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.8.2;
 
 contract ERC721 {
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+
     mapping(address => uint256) internal _balances;
     mapping(uint256 => address) internal _owners;
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
 
     function balanceOf(address owner) public view returns(uint256) {
         require(owner != address(0), "Address of owner cannot be zero!");
@@ -13,5 +16,14 @@ contract ERC721 {
         address owner = _owners[tokenId];
         require(owner != address(0), "Token ID does not exist!");
         return owner;
+    }
+
+    function setApprovalForAll(address operator, bool approved) public {
+        _operatorApprovals[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved); 
+    }
+
+    function isApprovedForAll(address owner, address operator) public view returns(bool) {
+        return _operatorApprovals[owner][operator];
     }
 }
