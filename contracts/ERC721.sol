6cca8ed55dd8d5c7fd1fe6fe71105b796da8cad6
mirkopezo <accfireover@tutanota.com>
2021-12-05 00:26:34 +0100

    Create ownerOf function

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -2,9 +2,16 @@ pragma solidity ^0.8.2;
 
 contract ERC721 {
     mapping(address => uint256) internal _balances;
-    
+    mapping(uint256 => address) internal _owners;
+
     function balanceOf(address owner) public view returns(uint256) {
         require(owner != address(0), "Address of owner cannot be zero!");
         return _balances[owner];
+    }
+
+    function ownerOf(uint256 tokenId) public view returns(address) {
+        address owner = _owners[tokenId];
+        require(owner != address(0), "Token ID does not exist!");
+        return owner;
     }
 }
