0bf8d19db37d4a5e47cd4ebb21b3aae187801cf8
mirkopezo <accfireover@tutanota.com>
2021-12-05 19:20:48 +0100

    Create tokenURI function

--- contracts/SuperMarioWorld.sol
+++ contracts/SuperMarioWorld.sol
@@ -6,9 +6,15 @@ contract SuperMarioWorld is ERC721 {
     string public name;
     string public symbol;
     uint256 public tokenCount;
+    mapping(uint256 => string) private _tokenURIs;
 
     constructor(string memory _name, string memory _symbol) {
         name = _name;
         symbol = _symbol;
+    }
+
+    function tokenURI(uint256 tokenId) public view returns(string memory) {
+        require(_owners[tokenId] != address(0), "Token ID does not exist!");
+        return _tokenURIs[tokenId];
     }
 }
