f539a73bec7fcccef41c86b100499f6efe118630
mirkopezo <accfireover@tutanota.com>
2021-12-05 01:28:11 +0100

    Create transferFrom and safeTransferFrom functions

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.2;
 contract ERC721 {
     event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
     event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
 
     mapping(address => uint256) internal _balances;
     mapping(uint256 => address) internal _owners;
@@ -39,5 +40,37 @@ contract ERC721 {
     function getApproved(uint256 tokenId) public view returns(address) {
         require(_owners[tokenId] != address(0), "Token ID does not exist!");
         return _tokenApprovals[tokenId];
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public {
+        address owner = ownerOf(tokenId);
+        address approved = getApproved(tokenId);
+        require(
+            msg.sender == owner || 
+            isApprovedForAll(owner, msg.sender) || 
+            msg.sender == approved, 
+            "You are not owner or approved for this transfer!"
+        );
+        require(from == owner, "From address is not the owner of token!");
+        require(to != address(0), "Recipient address cannot be zero!");
+        require(_owners[tokenId] != address(0), "Token ID does not exist!");
+        approve(address(0), tokenId);
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+        emit Transfer(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public {
+        transferFrom(from, to, tokenId);
+        require(_checkOnERC721Received(), "Receiver not implemented!");
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) public {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    function _checkOnERC721Received() private pure returns(bool) {
+        return true;
     }
 }
