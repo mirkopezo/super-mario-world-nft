a0989cad32329a2ecb4546aff5d5a5586ce30eb9
mirkopezo <accfireover@tutanota.com>
2021-12-05 01:12:07 +0100

    Create approve and getApproved functions

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.8.2;
 
 contract ERC721 {
+    event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
     event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
 
     mapping(address => uint256) internal _balances;
     mapping(uint256 => address) internal _owners;
     mapping(address => mapping(address => bool)) private _operatorApprovals;
+    mapping(uint256 => address) private _tokenApprovals;
 
     function balanceOf(address owner) public view returns(uint256) {
         require(owner != address(0), "Address of owner cannot be zero!");
@@ -25,5 +27,17 @@ contract ERC721 {
 
     function isApprovedForAll(address owner, address operator) public view returns(bool) {
         return _operatorApprovals[owner][operator];
+    }
+
+    function approve(address to, uint256 tokenId) public {
+        address owner = ownerOf(tokenId);
+        require(msg.sender == owner || isApprovedForAll(owner, msg.sender), "You are not owner or approved operator!");
+        _tokenApprovals[tokenId] = to;
+        emit Approval(owner, to, tokenId);
+    }
+
+    function getApproved(uint256 tokenId) public view returns(address) {
+        require(_owners[tokenId] != address(0), "Token ID does not exist!");
+        return _tokenApprovals[tokenId];
     }
 }
