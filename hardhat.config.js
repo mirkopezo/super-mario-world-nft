8affac68612010b415369f158988d38ec32cbee5
mirkopezo <accfireover@tutanota.com>
2021-12-06 17:34:43 +0100

    Fix two typos in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,8 +21,8 @@ module.exports = {
   solidity: "0.8.2",
   networks: {
     mumbai: {
-      url: process.env.MUMBAI_URL,
-      account: [ process.env.PRIVATE_KEY ]
+      url: process.env.MUMBAI_RPC,
+      accounts: [ process.env.PRIVATE_KEY ]
     }
   }
 };
