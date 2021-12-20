c2e052030c93419c81af9cc3c902fcc71ba3cff7
mirkopezo <accfireover@tutanota.com>
2021-12-20 11:11:22 +0100

    Add access control so only owner can mint tokens

--- contracts/erc721-nft-using-open-zeppelin/SuperMarioWorldOZ.sol
+++ contracts/erc721-nft-using-open-zeppelin/SuperMarioWorldOZ.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SuperMarioWorldOZ is ERC721URIStorage {
+contract SuperMarioWorldOZ is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -12,7 +13,7 @@ contract SuperMarioWorldOZ is ERC721URIStorage {
         ERC721(_name, _symbol)
     {}
 
-    function mint(string memory tokenURI) public returns (uint256) {
+    function mint(string memory tokenURI) public onlyOwner returns (uint256) {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
