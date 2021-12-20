e6c73de988a8316a2eec9bbec732ad47fca96403
mirkopezo <accfireover@tutanota.com>
2021-12-20 11:49:29 +0100

    Add more granular access control

--- contracts/erc721-nft-using-open-zeppelin/SuperMarioWorldOZ.sol
+++ contracts/erc721-nft-using-open-zeppelin/SuperMarioWorldOZ.sol
@@ -3,21 +3,39 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract SuperMarioWorldOZ is ERC721URIStorage, Ownable {
+contract SuperMarioWorldOZ is ERC721URIStorage, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
-    {}
+    {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
 
-    function mint(string memory tokenURI) public onlyOwner returns (uint256) {
+    function mint(string memory tokenURI)
+        public
+        onlyRole(MINTER_ROLE)
+        returns (uint256)
+    {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
